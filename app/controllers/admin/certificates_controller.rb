84d4f1d6a0d71c91337099e3a9452b389c88b3f3
Jun IIO <iiojun@aol.com>
2026-06-08 14:32:22 +0700

    trivial bugfix

--- app/controllers/admin/certificates_controller.rb
+++ app/controllers/admin/certificates_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CertificatesController < Admin::ApplicationController
   def index
-    @certificates = Certificate.all.order(id: :asc)
+    @certificates = Certificate.all.order(id: :desc)
     @certificates = Kaminari.paginate_array(@certificates).page(params[:page]).per(20)
   end
 end

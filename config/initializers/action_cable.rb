64814d9b17b855f67f0cefc5efb1e10eee2e4a9a
Jun IIO <iiojun@aol.com>
2026-07-06 21:33:01 +0900

    cable problem fixed?

--- config/initializers/action_cable.rb
+++ config/initializers/action_cable.rb
@@ -1,4 +1,10 @@
-# config/initializers/action_cable.rb
-
-# 通常3秒のPing間隔を1秒に設定
-ActionCable::Server::Base.config.ping_interval = 1
+module ActionCable
+  module Connection
+    class Base
+      # デフォルトの3秒から1秒に短縮して、Apacheが切断する前にパケットを送りつける
+      # (元の定数を上書きするため、警告が出ないように一度取り消してから再定義します)
+      remove_const(:BEAT_INTERVAL) if defined?(BEAT_INTERVAL)
+      BEAT_INTERVAL = 1.0
+    end
+  end
+end

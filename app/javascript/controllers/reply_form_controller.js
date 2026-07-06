c44a44338702e3e174954e0dfe7c6f2c4482a911
Jun IIO <iiojun@aol.com>
2026-07-06 16:29:57 +0900

    revert trivial modifies

--- app/javascript/controllers/reply_form_controller.js
+++ app/javascript/controllers/reply_form_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
   static targets = ["form", "button"]
 
   open() {
-    console.log("reply-form#open")
     this.formTarget.hidden = false
     this.buttonTarget.hidden = true
   }

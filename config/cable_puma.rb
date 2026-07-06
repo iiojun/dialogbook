fb4e92aab4decbf1bc60ca70c0fe08833bb21fc8
Jun IIO <iiojun@aol.com>
2026-07-06 16:57:28 +0900

    fix cable problem.

--- config/cable_puma.rb
+++ config/cable_puma.rb
@@ -1,10 +1,3 @@
-# Action Cable専用サーバ
 port ENV.fetch("CABLE_PORT", 28080)
 
 environment ENV.fetch("RAILS_ENV", "development")
-
-# Railsアプリそのものを読み込む
-require_relative "../config/environment"
-
-# Action CableサーバをRackとしてそのまま使う
-run ActionCable.server

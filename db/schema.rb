97cba7b4ee0439035e79cae24873322a63e7145d
Jun IIO <iiojun@aol.com>
2026-08-26 11:46:05 +0700

    make time zone not null.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_08_25_052923) do
+ActiveRecord::Schema[8.0].define(version: 2026_08_26_044349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_08_25_052923) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "time_zone"
+    t.string "time_zone", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -108,7 +108,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_08_25_052923) do
     t.boolean "need_response", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "time_zone"
+    t.string "time_zone", null: false
     t.index ["lesson_id"], name: "index_posts_on_lesson_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
